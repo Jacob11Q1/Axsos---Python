867a089587216ad9f8cd091a76bd9a9edd8881e7
Jacob Qumsiyeh <38016471+Jacob11Q1@users.noreply.github.com>
2025-07-18 11:36:49 +0300

    Add files via upload

--- Assignments/For-Loops-Basics1/for-loops-basics1.py
+++ Assignments/For-Loops-Basics1/for-loops-basics1.py
@@ -3,10 +3,16 @@ for b in range(0 , 151):
     print(b);
     
 # Multiples Of 5 from 5 to 1000:
-for m in range(5 , 1000+5):
-    if m % 5 == 0:
-        print(m);
-    
+    # another Example: # Note: Teacher Shahd Told Me About This Technique
+for i in range(5 , 1001 ,5):
+    print(i)
+
+# for m in range(5 , 1000+5):
+#     if m % 5 == 0:
+#         print(m);
+
+
+
 # Counting The Dojo Way:
 for c in range(1 , 101):
     if c % 10 == 0:
@@ -17,15 +23,25 @@ for c in range(1 , 101):
         print(c);
 
 # Whoa. That Sucker's Huge (Add Odd Integers from 0 to 500000) and print thier sum:
-sum = 0;
-for i in range(0,500000+1):
-    if i % 2 == 1:
-        sum = sum + i;
-print("The Sum is: " , sum);
+    # Another Example: # Note: Teacher Shahd Told Me About This Technique
+sum = 0
+for i in range(1 , 500000 ,2):
+    sum += i
+print(sum)
+
+
+# sum = 0;
+# for i in range(0,500000+1):
+#     if i % 2 == 1:
+#         sum = sum + i;
+# print("The Sum is: " , sum);
 
 
 # Flexible Counter:
-mult = 3;
-for i in range(9 , 0 , -1):
+# 6.flexible counter
+lowNum=2
+highNum=9
+mult=3
+for i in range(highNum,lowNum,-1):
     if i % mult == 0:
-        print(i);
+        print(i)
